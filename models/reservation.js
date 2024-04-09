65eca40607fad8a116ffc211ffeddadd9a30d98b
Leianni0 <leianniwalker0@gmail.com>
2024-04-08 20:53:21 -0700

    Added review model and controller

---
+++ models/reservation.js
@@ -0,0 +1,3 @@
+//Represents individual reservations made by users for specific restaurants and timeslots.
+//Contains fields such as user ID (foreign key), restaurant ID (foreign key), reservation date and time, party size, and status (e.g., confirmed, canceled).
+//May include additional fields for special requests, reservation notes, or payment information
---
+++ models/user.js
@@ -0,0 +1,3 @@
+//Represents user accounts in the system.
+//Contains fields such as username, email, password (hashed), profile information, and possibly roles or permissions.
+//May include additional fields for user preferences, settings, or authentication tokens.
