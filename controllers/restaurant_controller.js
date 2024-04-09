187f558918d7f408e7404dd2896e973395f4280a
Leianni0 <leianniwalker0@gmail.com>
2024-04-08 20:48:19 -0700

    Added Controllers and Models

---
+++ controllers/restaurant_controller.js
@@ -0,0 +1,3 @@
+//Handles restaurant-related actions such as fetching restaurant details, listing restaurants, and searching for restaurants.
+//Operations like retrieving restaurant information, updating restaurant details, and handling restaurant reviews if applicable.
+//Interacts with the restaurant model to perform CRUD operations on restaurant data.
---
+++ controllers/user_controller.js
@@ -0,0 +1,3 @@
+//Manages user-related actions such as user registration, login, logout, and profile management.
+//Handles user authentication and authorization, ensuring secure access to application resources.
+//Validates user input and interacts with the user model for data retrieval and manipulation
