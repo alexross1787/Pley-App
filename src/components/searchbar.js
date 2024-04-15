fbb9db04bed741839fba385e71b6155ff267029e
Anthony <anthonylsa12@gmail.com>
2024-04-15 00:21:31 -0700

    Searchbar partially works. console.log indicates that pressing enter or clicking on search functions. console will return a message that says "Searchin for *insert search term here*" Need to integrate the API key to have the searchbar return businesses from the Yelp Database

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css/navbar.css";
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log("Searching for:", searchTerm);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return ( 
     <div className='searchbar-container'>
-    <div className='input-group input-group-md'> 
-    <div className='input-group-prepend'>
-
+      <div className='input-group input-group-md'> 
+        <input type='text' className='form-control' aria-label='Large' aria-describedby='inputGroup-sizing-sm' value={searchTerm} onChange={handleInputChange} onKeyPress={handleKeyPress} />
+        <div className='input-group-append'>
+          <button className='btn btn-outline-secondary' type='button' onClick={handleSearch}>
+            Search
+          </button>
+        </div>
       </div>
-      <input type='text' className='form-control' aria-label='Large' aria-describedby='inputGroup-sizing-sm'></input>
-<span className='input-group-text' id='inputGroup-sizing-lg'>Search</span>
     </div>
-    </div>
-  )
+  );
 }
