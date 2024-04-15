82d3011edf0ed05dac696e636e2207a366910266
Anthony <anthonylsa12@gmail.com>
2024-04-15 13:52:15 -0700

    updated searchFunctions.js

--- src/api/searchFunction.js
+++ src/api/searchFunction.js
@@ -1,29 +1,6 @@
 // This file allows the user to search for businesses
 // Businesses that are returned should be from the Yelp Database
 
-// import { useState, useEffect } from 'react';
-// import * as api from './yelp-api'
-
-// export function searchFunction(term, location) {
-//     const [restaurants, setRestaurants] = useState([]);
-//     const [searchParams, setSearchParams] = useState({ term, location })
-
-//     useEffect (() => {
-//         setRestaurants([]);
-//         const fetchData = async () => {
-//             try {
-//                 const yelpData = await api.get('/businesses/search', searchParams)
-//                 const parse = await yelpData.json
-//                 setRestaurants(parse.businesses)
-//             } catch(err) {
-//                 console.log('Could not fetch data.')
-//             }
-//         }
-//         fetchData();
-//     }, [term, location, searchParams])
-//     return [restaurants, searchParams, setSearchParams]
-// }
-
 import { useState, useEffect } from 'react';
 import * as api from './yelp-api';
 
@@ -48,6 +25,7 @@ export function searchFunction(term, location) {
             }
         };
         fetchData();
+        // searchParams is dependency array. Will update whenever value of searchParams changes
     }, [searchParams]);
 
     return [restaurants, searchParams, setSearchParams];
