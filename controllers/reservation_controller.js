dea4ad9ab92dd570fddab9808f78fddbb464ae10
Leianni0 <leianniwalker0@gmail.com>
2024-04-15 21:15:19 -0700

    Controllers

--- controllers/reservation_controller.js
+++ controllers/reservation_controller.js
@@ -1,6 +1,3 @@
-//creating, updating, and canceling reservations.
-//Validates*** reservation requests and checks availability based on restaurant capacity and existing reservations.
-//Interacts with the reservation model to store and retrieve reservation data.
 
 //DEPENDENCIES
 const express = require('express');
