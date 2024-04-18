01df53e69a3bc3178e2f1d4a32271f427692ec24
Anthony <anthonylsa12@gmail.com>
2024-04-18 13:42:10 -0700

    cleaning up restaurant form

--- src/components/forms/restaurant_form.js
+++ src/components/forms/restaurant_form.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 export default function RestaurantForm() {
+    const [name, setName] = useState(''); 
+    const [address, setAddress] = useState('');
+    const [cuisine, setCuisine] = useState('');
+    const [capacity, setCapacity] = useState('');
     return (
         <div class="container">
-        <form id="restaurantForm">
-            <input type="text" placeholder="Restaurant Name" class="form-label" id="name" required></input>
-            <input type="text" placeholder="Address" class="form-label" id="address" required></input>
-            <input type="text" placeholder="Cuisine" class="form-label" id="cuisine" required></input>
-            <input type="number" placeholder="Capacity" class="form-label" id="capacity" required></input>
+        <form id="restaurantForm" onSubmit={(e) => e.preventDefault()}>
+            <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Restaurant Name" class="form-label" id="name" required></input>
+
+            <input value={address} onChange={(e) => setAddress(e.target.value)} type="text" placeholder="Address" class="form-label" id="address" required></input>
+
+            <input value={cuisine} onChange={(e) => setCuisine(e.target.value)} type="text" placeholder="Cuisine" class="form-label" id="cuisine" required></input>
+
+            <input value={capacity} onChange={(e) => setCapacity(e.target.value)} type="number" placeholder="Capacity" class="form-label" id="capacity" required></input>
+
             <button type="submit" class="btn btn-primary">submit</button>
         </form>
         </div>
