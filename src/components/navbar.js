dfe18d2177bac3f68ef9c02363ab18e38aacb272
alexross1787 <alexross1787@gmail.com>
2024-04-18 22:36:52 -0700

    fixed nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,19 +8,23 @@ export default function Navbar() {
     return (
         <div className='sticky-navbar'> 
             <nav className='navbar navbar-expand-sm navbar-light bg-custom'> 
+            <div className='nav-top'>
+         
                 <img className="pley-logo" src="/pley-logo.png" alt="Pley Logo" />
-                <div className='nav-searchbar-container'>
+                
+                
                     <div className="tagline-container">
                         <p className="tagline" >Skip the wait, reserve your plate</p>
                     </div>
                         <SearchBar />
                 </div>
-                <div className='links'>
+                
+                <div className='nav-links'>
                     <div className='row'>
-                        <div className='col 4'>Home</div>
-                        <div className='col 4'>Reservations</div>
-                        <div className='col 4'>Reviews</div>
-                        <div className='col 4'>My Account</div>
+                        <div className='col 3'>Home</div>
+                        <div className='col 3'>Reservations</div>
+                        <div className='col 3'>Reviews</div>
+                        <div className='col 3'>My Account</div>
                     </div>
                 </div>
              </nav>
---
+++ src/views/default.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+
+function Def() {
+    return (
+        <div>
+    <Navbar />
+    <Footer />
+
+
+    </div>
+)
+
+}
+
+export default Def;
