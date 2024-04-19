bcefe175c321a14ff7a7b790ab2e5a5b4ab02a04
Anthony <anthonylsa12@gmail.com>
2024-04-19 00:59:23 -0700

    Created form templates for users and reviews

---
+++ src/components/forms/user _form.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { createNewRestaurant } from "../../store/restaurant";
+
+export default function RestaurantForm() {
+    const [name, setName] = useState(''); 
+    const [address, setAddress] = useState('');
+    const [cuisine, setCuisine] = useState('');
+    const [capacity, setCapacity] = useState('');
+    const handleSubmit = () => {
+        dispatchEvent(createNewRestaurant()).then((id) =>{
+            {name, address, cuisine, capacity}
+        })
+    }
+    return (
+        <div class="container">
+        <form id="restaurantForm" onSubmit={(e) => e.preventDefault()}>
+            <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Restaurant Name" class="form-label" id="name" required></input>
+
+            <input value={address} onChange={(e) => setAddress(e.target.value)} type="text" placeholder="Address" class="form-label" id="address" required></input>
+
+            <input value={cuisine} onChange={(e) => setCuisine(e.target.value)} type="text" placeholder="Cuisine" class="form-label" id="cuisine" required></input>
+
+            <input value={capacity} onChange={(e) => setCapacity(e.target.value)} type="number" placeholder="Capacity" class="form-label" id="capacity" required></input>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    )
+}
