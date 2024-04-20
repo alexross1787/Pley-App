351d29d9e108f0422403e9291354485052c232d8
alexross1787 <alexross1787@gmail.com>
2024-04-20 13:23:06 -0700

    fixed landing page issue

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -2,15 +2,25 @@ import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css/restaurant.css"
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Restaurant() {
-  
+  // const [restaurant, setRestaurant] = useState(null);
 
+  // useEffect(() => {
+  //   const fetchRestaurantData = async () => {
+  //     try {
+  //       const response = await axios.get('../api/models/restaurant.js')
+        
+  //       setRestaurant(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching restaurant data:', error);
+  //     }
+  //   };
 
-  useEffect(() => {
-    dispatch(getRestaurant(restaurantId));
-  }, [dispatch, restaurantId]);
-
+  //   // Call the fetchRestaurantData function
+  //   fetchRestaurantData();
+  // }, []); 
 
     return (
         <div>
@@ -20,7 +30,7 @@ export default function Restaurant() {
         </div>
         <div className="card-body">
           <h5 className="card-title ">Restaurant Name</h5>
-          <p className="card-text"> Rating{}/ (amount of reviews) /Distance/ Price </p>
+          <p className="card-text"> Rating{} /Distance/ Price </p>
           <a href="/newRestaurant" className="btn ">More</a>
         </div>
       </div> 
