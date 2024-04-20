d08648f9a2157c6389b532e860aac1a92c0b6539
Anthony <anthonylsa12@gmail.com>
2024-04-20 14:06:52 -0700

    syncing

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,29 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css/restaurant.css"
-<<<<<<< HEAD
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-
-export default function Restaurant() {
-  // const [restaurant, setRestaurant] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchRestaurantData = async () => {
-  //     try {
-  //       const response = await axios.get('../api/models/restaurant.js')
-        
-  //       setRestaurant(response.data);
-  //     } catch (error) {
-  //       console.error('Error fetching restaurant data:', error);
-  //     }
-  //   };
-
-  //   // Call the fetchRestaurantData function
-  //   fetchRestaurantData();
-  // }, []); 
-
-=======
 import { useParams } from 'react-router-dom';
 import { getRestaurant } from '../store/restaurant';
 import { useDispatch, useSelector } from 'react-redux';
@@ -39,7 +16,6 @@ export default function Restaurant() {
 
   if (!state || !state.restaurant) return null;
   const restaurant = state.restaurant
->>>>>>> e637e2c2f6de905f5f8292cb29228fa2a7fb089d
     return (
         <div>
         <div className="rest-card">
@@ -47,13 +23,8 @@ export default function Restaurant() {
         <img className="card-img-top" src="/restaurant-sample-pic.jpg" alt="Card image cap"/>
         </div>
         <div className="card-body">
-<<<<<<< HEAD
-          <h5 className="card-title ">Restaurant Name</h5>
-          <p className="card-text"> Rating{} /Distance/ Price </p>
-=======
           <h5 className="card-title ">{restaurant.name}</h5>
           <p className="card-text"> {restaurant.address}/ {restaurant.cuisine} / {restaurant.capacity} </p>
->>>>>>> e637e2c2f6de905f5f8292cb29228fa2a7fb089d
           <a href="/newRestaurant" className="btn ">More</a>
         </div>
       </div> 
