eb94f70f79b67de1e0b36833eae95e38779c40d7
Anthony <anthonylsa12@gmail.com>
2024-04-20 13:08:29 -0700

    finishing up

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,17 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles.css/restaurant.css"
-import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { getRestaurant } from '../store/restaurant';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Restaurant() {
-  
-
+  const dispatch = useDispatch();
+  const { restaurantId } = useParams();
+  const state = useSelector((state) => state)
 
   useEffect(() => {
     dispatch(getRestaurant(restaurantId));
   }, [dispatch, restaurantId]);
 
-
+  if (!state || !state.restaurant) return null;
+  const restaurant = state.restaurant
     return (
         <div>
         <div className="rest-card">
@@ -19,8 +23,8 @@ export default function Restaurant() {
         <img className="card-img-top" src="/restaurant-sample-pic.jpg" alt="Card image cap"/>
         </div>
         <div className="card-body">
-          <h5 className="card-title ">Restaurant Name</h5>
-          <p className="card-text"> Rating{}/ (amount of reviews) /Distance/ Price </p>
+          <h5 className="card-title ">{restaurant.name}</h5>
+          <p className="card-text"> {restaurant.address}/ {restaurant.cuisine} / {restaurant.capacity} </p>
           <a href="/newRestaurant" className="btn ">More</a>
         </div>
       </div> 
